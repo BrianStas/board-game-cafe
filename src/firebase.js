e724ac3951c74de498ba7c146050ce4ddd4ed566
Brian Stasiukaitis <brianstas@gmail.com>
2024-06-03 16:47:23 -0400

    installed firebase
    Hopefully installed firebase and initialized firestore database. Learned to hide all the important info in a .env file, though haven't attempted to initialize app since it wouldn't display anything as of now.

---
+++ src/firebase.js
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: "board-game-cafe-a5978.firebaseapp.com",
+  projectId: "board-game-cafe-a5978",
+  storageBucket: "board-game-cafe-a5978.appspot.com",
+  messagingSenderId: "1091605199751",
+  appId: "1:1091605199751:web:4bec3cb0d995b395387122",
+  measurementId: "G-BR7CVEVMHY"
+};
+
+
+// Initialize Firebase
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
