96a9a9d54dc69d5406a23b9ab56d1d25b4b5d938
Brian Stasiukaitis <brianstas@gmail.com>
2024-07-27 23:41:05 -0400

    comment made
    made a comment. Gotta keep that github streak alive

--- src/context/ShoppingCart.js
+++ src/context/ShoppingCart.js
@@ -2,6 +2,8 @@ import React, { useState } from 'react'
 
 function ShoppingCart() {
   const {closeCart} = useShoppingCart()
+
+  //working is tough sometimes
   return (
     <div className="drawer drawer-end">
         <input id="my-drawer-4" type="checkbox" className="drawer-toggle" />
