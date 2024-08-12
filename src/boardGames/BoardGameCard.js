87f37b79236dfdf75a3cfb08f8d016334e111f78
Brian Stasiukaitis <brianstas@gmail.com>
2024-08-11 23:36:03 -0400

    added comments
    added comments to get my github commit. It be like that sometimes

--- src/boardGames/BoardGameCard.js
+++ src/boardGames/BoardGameCard.js
@@ -1,13 +1,14 @@
 import React from 'react'
 
 function BoardGameCard({game}) {
+
+  // update for the github heyyyy
   return (
     <>
         <div className="card lg:card-side h-60 bg-base-100 shadow-xl card-bordered grid grid-cols-12 grid-rows-3 grid-flow-cols">
             <figure className=" col-span-3 row-span-3"><img src={game.imgurl} alt="Movie" className='max-h-full object-contain' /></figure>
             <div className="card-body col-span-8 row-span-3">
                 <h2 className="card-title justify-center">{game.name}</h2>
-                {/* need to adjust for card length so it doesn't run over */}
                 <p>{game.description}</p>
                 <div className="card-actions justify-end">
                 <button className="btn btn-secondary">Learn To Play</button>
