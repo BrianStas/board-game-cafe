2791a5c93cee91031ba8fc4ef9b94662b0de8acf
Brian Stasiukaitis <brianstas@gmail.com>
2024-10-26 23:24:07 -0400

    making commit
    commiting with comment

--- src/boardGames/BoardGamePage.js
+++ src/boardGames/BoardGamePage.js
@@ -13,8 +13,6 @@ function BoardGamePage() {
        .then(data => setBgd(data))
     }
 
-    //need a commit before I start new products
-
     return (<div className="max-w-5xl mx-auto">
         <div className="flex justify-center">
         <iframe 
