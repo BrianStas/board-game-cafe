86bcaf52841edac21a800a26f10079b8cf6f1d4e
Brian Stasiukaitis <brianstas@gmail.com>
2024-11-04 23:23:23 -0500

    commit for hub
    comment for hub color, but I committed multiple times to private project

--- src/food/FoodCard.js
+++ src/food/FoodCard.js
@@ -9,6 +9,8 @@ function FoodCard({food}) {
   const cartItem = cartItems.find(cartItem => cartItem.id === food.id);
   const quantityInCart = cartItem ? cartItem.quantity : 0;
 
+  //working on internship posts, but they are unseen for github
+
   return (
     <>
         <div className="card bg-base-100 w-64 shadow-xl text-center h-full">
