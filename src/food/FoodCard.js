48c508621853948ccabfbfebf1538ccfd7e24932
Brian Stasiukaitis <brianstas@gmail.com>
2024-11-29 15:05:32 -0500

    updated
    As mentioned, this is in place of the NDA I'm working under for the actual project. Please do not count me out for these silly commits.

--- src/food/FoodCard.js
+++ src/food/FoodCard.js
@@ -9,8 +9,6 @@ function FoodCard({food}) {
   const cartItem = cartItems.find(cartItem => cartItem.id === food.id);
   const quantityInCart = cartItem ? cartItem.quantity : 0;
 
-  //this is another commit for the week
-
   return (
     <>
         <div className="card bg-base-100 w-64 shadow-xl text-center h-full">
