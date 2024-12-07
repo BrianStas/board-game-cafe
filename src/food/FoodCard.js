2b94338299b936f0f518ee811b44411e6c23f796
Brian Stasiukaitis <brianstas@gmail.com>
2024-12-06 22:23:01 -0500

    here we are
    once again

--- src/food/FoodCard.js
+++ src/food/FoodCard.js
@@ -9,6 +9,8 @@ function FoodCard({food}) {
   const cartItem = cartItems.find(cartItem => cartItem.id === food.id);
   const quantityInCart = cartItem ? cartItem.quantity : 0;
 
+  //i lied, still doing it
+
   return (
     <>
         <div className="card bg-base-100 w-64 shadow-xl text-center h-full">
