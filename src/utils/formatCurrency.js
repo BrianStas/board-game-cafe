ec6ac435a38fd03bcb551b54ecf98cc2b6cc7094
Brian Stasiukaitis <brianstas@gmail.com>
2024-12-30 23:53:02 -0500

    clarification
    more comment clarification

--- src/utils/formatCurrency.js
+++ src/utils/formatCurrency.js
@@ -1,5 +1,5 @@
 const CURRENCY_FORMATTER = new Intl.NumberFormat(undefined, {currency: "USD", style: "currency"})
-
+// This correctly defines numbers into 2 decimal currency format
 export function formatCurrency(number){
     return CURRENCY_FORMATTER.format(number)
 }
