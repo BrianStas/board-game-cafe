89caf07188b47661bf5c04fcb6d800a061041386
Brian Stasiukaitis <brianstas@gmail.com>
2025-02-21 10:45:14 -0500

    yep
    for sure did it

--- src/layout/SearchBar.js
+++ src/layout/SearchBar.js
@@ -9,6 +9,7 @@ function SearchBar({games}) {
     const [searchInput, setSearchInput] = useState("");
     const navigate = useNavigate();
  
+    //for some reason my work on the new fork of a project isn't counting so here we are.
 
     const options = games.map((game) => {
         return { value: game.id, label: game.name }
