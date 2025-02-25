15e4058728acf61e19d892fd819e90a9d74d68c1
Brian Stasiukaitis <brianstas@gmail.com>
2025-02-26 07:34:33 +0900

    removed comment
    removed comment

--- src/layout/SearchBar.js
+++ src/layout/SearchBar.js
@@ -4,8 +4,6 @@ import Select from 'react-select'
 
 //react-select is a specific react library for this functionality
 
-//waiting on the other project to merge my branch and then going back to that project.
-
 function SearchBar({games}) {
 
     const [searchInput, setSearchInput] = useState("");
