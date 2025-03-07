f55b035444d573ca6dba7cc4169d372770de5ec5
Brian Stasiukaitis <brianstas@gmail.com>
2025-03-07 19:16:42 +0900

    coding
    this counts as coding

--- src/food/FoodHome.js
+++ src/food/FoodHome.js
@@ -4,6 +4,8 @@ import FoodCard from './FoodCard';
 
 //this component is for the main food page, showing all items in the menu
 
+//extending my streak
+
 function FoodHome() {
     const [loading, setLoading] = useState(true);
     const [menuItems, setMenuItems] = useState([]);
