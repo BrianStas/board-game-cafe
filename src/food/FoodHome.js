62dbaa57660fad00b9700313a4d21e66681be62e
Brian Stasiukaitis <brianstas@gmail.com>
2025-03-16 19:34:48 +0900

    removed comment
    Removed the comment. Still working on private project

--- src/food/FoodHome.js
+++ src/food/FoodHome.js
@@ -4,8 +4,6 @@ import FoodCard from './FoodCard';
 
 //this component is for the main food page, showing all items in the menu
 
-//adding to new project for actual money but the private contributions dont count
-
 function FoodHome() {
     const [loading, setLoading] = useState(true);
     const [menuItems, setMenuItems] = useState([]);
