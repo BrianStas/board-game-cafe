5e38ea326d6ed767c3ed8cda75ae64ee5fd1a115
Brian Stasiukaitis <brianstas@gmail.com>
2025-03-18 23:19:28 +0900

    need to juggle
    keeping this rolling so I get credit for my private commits

--- src/food/FoodHome.js
+++ src/food/FoodHome.js
@@ -4,6 +4,8 @@ import FoodCard from './FoodCard';
 
 //this component is for the main food page, showing all items in the menu
 
+//this is a comment to keep up my github
+
 function FoodHome() {
     const [loading, setLoading] = useState(true);
     const [menuItems, setMenuItems] = useState([]);
