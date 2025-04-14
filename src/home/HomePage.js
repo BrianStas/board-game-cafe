cf60e1d03cf6f8e73c8371bc80dbe42381d14525
Brian Stasiukaitis <brianstas@gmail.com>
2025-04-14 21:56:11 +0900

    made comment
    Making my commit for the day

--- src/home/HomePage.js
+++ src/home/HomePage.js
@@ -6,7 +6,7 @@ import { motion } from "framer-motion";
 
 //TODO: Find new button formats and real design elements
 
-
+//testing out Github Copilot
 
 export default function HomePage() {
   const navigate = useNavigate();
