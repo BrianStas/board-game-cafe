3ccc0ee658f73ce2920e57ec230a74aee5e61673
Brian Stasiukaitis <brianstas@gmail.com>
2025-08-24 15:36:01 +0900

    updated
    done

--- src/boardGames/BoardGameList.js
+++ src/boardGames/BoardGameList.js
@@ -9,8 +9,6 @@ function BoardGameList() {
     // eslint-disable-next-line react-hooks/exhaustive-deps
     useEffect(loadGames, []);
 
-    //got it done
-
     function loadGames(){
         listGames()
         .then(setBoardGames)
